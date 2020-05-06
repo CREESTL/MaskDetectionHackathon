bb0238fda37356b00d3c1ac1d7c4e894ffce2872
CREESTL <kroymw3@yandex.ru>
2020-05-06 23:33:27 +0300

    Закончил всю работу с файлом super_modify_obj_folder

--- useful_programs/super_modify_obj_folder.py
+++ useful_programs/super_modify_obj_folder.py
@@ -10,6 +10,7 @@
 """
 import os
 import time
+from shutil import copyfile, copy
 
 # Функция меняет расширение всех изображений на .jpg
 def rename_to_jpg(path):
@@ -63,71 +64,60 @@ def delete_invalid_files(invalid_files, path):
         if choice == "y":
             for file in invalid_files:
                 if file in os.listdir(path):
-                    print("Удаляю файл: " + path + r"\\" + file)
+                    print("Удаляю файл: ", file)
                     os.remove(path + r"\\" + file)
         else:
             print("Ну ладно")
     else:
-        print("Всё в порядке!")
+        print("Для каждого файла существует действительная разметка. Все в порядке!")
 
 
 # функция нумерует все оставшиеся файлы в формате 000001 в порядке возрастания
 def set_numbers(path):
-    choice = str(input("\nХотите ли вы пронумеровать все файлы в порядке возрастания?(y/n)\n"))
-    if choice == "y":
-        start = int(input("С какого числа начать нумерацию?\n"))
-        j = start
-        files = os.listsdir(path)
-        print("files:")
-        for file in files: print(file)
-        for i, file in enumerate(files):
-            extention = file[-4:]
-            new_name = "{:06}".format(j) + extention
-            print("new name = ", new_name)
-            if new_name in os.listdir(path):
-                differense = files.index("{:06}".format(j) + extention) - files.index(file)  # считаем разницу индексов между тем, на котором возникла ошибка и тем, который уже существует
-                print(new_name + " is in " + str(os.listdir(path)))
-                # Если файл с только что созданным именем существует, то мы его переименовываем через разность индексов
-                print( path + r'\\'+ new_name, " rename to ", path + r"\\" + "{:06}".format(j + differense) + extention)
-                os.rename(path + r'\\'+ new_name, path + r"\\" + "{:06}".format(j + differense) + extention)
-                # А затем переименовываем наш исходный файл
-                print(path + r"\\" + file, " rename to ", new_name)
-                os.rename(path + r"\\" + file, new_name)
-            else:
-                # Если все в порядке, то просто переименовываем исходный файл и все
-                os.rename(path + r"\\" + file, new_name)
-
-
-
-
-                '''
-                print()
-                print(file, "1rename to", "{:06}".format(j) + extention)
-                os.rename(path + r"\\" + file, path + r"\\" + "{:06}".format(j) + extention)
-                if i % 2 != 0:
-                    j += 1
-            # Если в папке, например 300 фото, начиная с 000001.jpg, а надо пронумеровать их с 200 и больше.
-            # то он попробует перименовать 0000001 в 000221, которая уже есть - это нарушает работу
-            except FileExistsError:
-                print("last index = ", files.index("{:06}".format(j) + extention))
-                print("first index = ", files.index(file))
-                differense = files.index("{:06}".format(j) + extention) - files.index(file) # считаем разницу индексов между тем, на котором возникла ошибка и тем, который уже существует
-                print("DIF = ", differense)
-                # прибавляем эту разницу к существующему файлу.
-                print("{:06}".format(j) + extention, " rename to ", "{:06}".format(j + differense) + extention)
-                os.rename(path + r"\\" + "{:06}".format(j) + extention, path + r"\\" + "{:06}".format(j + differense) + extention)
-            except FileNotFoundError:
-                # Если мы существующий файл перенумеровали, то он все еще остался в старом os.listdir(), поэтому его надо игнорировать там
-                continue
-                '''
-        print("Файлы были пронумерованы в порядке возрастания!")
-    else:
-        print("Ну ладно, пока!")
+    start = int(input("С какого числа начать нумерацию?\n"))
+    j = start
+    files = os.listdir(path)
+    if "temp" in files:
+        files.remove("temp")
+    # Создаем резервную папку
+    try:
+        os.makedirs(path + '/temp')
+    except:
+        pass
+    for i, file in enumerate(files):
+        extention = file[-4:]
+        copy(path + "/" + file, path + "/temp/") # Копируем файл в резервную папку
+        # Переименовываем файлы в резервной папке в соответствии с номером, который ввёл пользователь
+        if i % 2 != 0:
+            os.rename(path + "/temp/" + file, path + "/temp/" + "{:06}".format(j) + extention)
+            j += 1
+        else:
+            os.rename(path + "/temp/" + file, path + "/temp/" + "{:06}".format(j) + extention)
+        os.remove(path + "/" + file) # из исходной папки мы их удаляем
+    new_files = os.listdir(path + "/temp/")
+    for file in new_files:
+        os.rename(path + "/temp/" + file, path + "/" + file) # а теперь обратно из резервной папки копируем файлы в исходную
+    os.rmdir(path + "/temp/") # удаляем резервную папку
+    print("Файлы были пронумерованы в порядке возрастания!")
 
 ############################################################################
 path = str(input("Введите путь к папке с фотографиями и .txt файлами: \n"))
-invalid_files = rename_to_jpg(path)
-check_if_txt_exists(path, invalid_files)
-delete_invalid_files(invalid_files, path)
-set_numbers(path)
-
+while True:
+    print("\nПожалуйста, выберите, что хотите сделать: ")
+    print("1) Проверить правильность разметки (наличие корректного текстового файла для каждого изображения)")
+    print("2) Пронумеровать файлы в порядке возрастания")
+    print("3) Выйти")
+    print("Выш выбор: ", end="")
+    choice = int(input())
+    print('\n')
+    if choice == 1:
+        invalid_files = rename_to_jpg(path)
+        invalid_files = check_if_txt_exists(path, invalid_files)
+        delete_invalid_files(invalid_files, path)
+        time.sleep(1)
+    elif choice == 2:
+        set_numbers(path)
+        time.sleep(1)
+    else:
+        print("Пока!")
+        break
